71f4e9a3060feeb8580b92ef3cb4debadd19367d
Diana <Khachaturova.Diana@student.greenriver.edu>
2025-02-06 14:51:55 -0800

    Worked on practice

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,5 +1,41 @@
 public class Traversal {
   public static void main(String[] args) {
 
+    TreeNode root = new TreeNode(77, null, null);
+    root.left = new TreeNode(22, null, null);
+    root.right = new TreeNode(86, null, null);
+    root.left.right = new TreeNode(33, null, null);
+
+    // System.out.println(root.left.right.value); 
+    // preOrder(root);
+    // inOrder(root);
+    postOrder(root);
+  }
+
+  public static void preOrder(TreeNode node) {
+    if (node == null) {
+      return;
+    }
+    System.out.println(node.value);
+    preOrder(node.left);
+    preOrder(node.right);
+  }
+
+  public static void inOrder(TreeNode node) {
+    if (node == null) {
+      return;
+    }
+    preOrder(node.left);
+    System.out.println(node.value);
+    preOrder(node.right);
+  }
+
+  public static void postOrder(TreeNode node) {
+    if (node == null) {
+      return;
+    }
+    preOrder(node.left);
+    preOrder(node.right);
+    System.out.println(node.value);
   }
 }
--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,4 +1,12 @@
 public class TreeNode {
-
+    int value;
+    TreeNode left;
+    TreeNode right;
   
+    public TreeNode(int value, TreeNode left, TreeNode right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+    
 }
