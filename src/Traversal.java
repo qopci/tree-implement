10af519a234fe13c82de76704ac7bd8d2800966b
Diana <Khachaturova.Diana@student.greenriver.edu>
2025-02-13 14:48:06 -0800

    Day 3

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,10 +1,12 @@
+import java.util.Stack;
+
 public class Traversal {
   public static void main(String[] args) {
 
     // Int Tree
     TreeNode<Integer> root = new TreeNode<>(77, null, null);
     root.left = new TreeNode<>(22, null, null);
-    root.right = new TreeNode<>(86, null, null);
+    root.right = new TreeNode<>(87, null, null);
     root.right.left = new TreeNode<>(9, null, null);
     root.left.right = new TreeNode<>(33, null, null);
     root.left.right.left = new TreeNode<>(86, null, null);
@@ -14,7 +16,17 @@ public class Traversal {
     stringRoot.left = new TreeNode<>("hi", null, null);
     stringRoot.right = new TreeNode<>("hey", null, null);
 
-    preOrder(stringRoot);
+    // System.out.println(countLevels(root));
+
+    TreeNode<Integer> megaRoot = new TreeNode<>(0, null, null);
+
+    TreeNode<Integer> current = megaRoot;
+    for (int i = 1; i <= 999999; i++) {
+      current.left = new TreeNode<>(i, null, null);
+      current = current.left;
+    }
+
+    preOrderIter(root);
 
     // preOrder(root);
     // inOrder(root);
@@ -22,6 +34,23 @@ public class Traversal {
     // greaterThan(root, 25);
     // int result = countNodes(root);
     // System.out.println(result);
+  }
+
+  public static <T> void preOrderIter(TreeNode<T> node) {
+    Stack<TreeNode<T>> nodeStack = new Stack<>();
+
+    nodeStack.push(node);
+
+    while (!nodeStack.empty()) {
+      TreeNode<T> current = nodeStack.pop();
+
+      if (current == null) {
+        continue;
+      }
+      System.out.println(current.value);
+      nodeStack.push(current.right);
+      nodeStack.push(current.left);
+    }
   }
 
   public static <T> void preOrder(TreeNode<T> node) {
@@ -68,5 +97,27 @@ public class Traversal {
       return 0;
     }
     return 1 + countNodes(node.left) + countNodes(node.right);
+  }
+
+  public static <T> int countLevels(TreeNode<T> node) {
+    if (node == null) {
+      return 0;
+    }
+    // height of larger subtree + 1
+    int leftHeight = countLevels(node.left);
+    int rightHeight = countLevels(node.right);
+    int biggerHeight = Math.max(leftHeight, rightHeight);
+
+    return biggerHeight + 1;
+  }
+
+  // null -> true
+  public static boolean allOdd(TreeNode<Integer> node) {
+    if (node == null) {
+      return true;
+    }
+    return node.value % 2 != 0 &&
+          allOdd(node.left) &&
+          allOdd(node.right);
   }
 }
